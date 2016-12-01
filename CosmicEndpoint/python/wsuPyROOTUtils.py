eec101f3915f9a628735c69e8286382fb1d5c97a
Jared Sturdy <sturdy@cern.ch>
2016-12-01 12:18:56 +0100

    updates to utilities

--- CosmicEndpoint/python/wsuPyROOTUtils.py
+++ CosmicEndpoint/python/wsuPyROOTUtils.py
@@ -1,5 +1,70 @@
 import ROOT as r
 
+# keep a pointer to the original TCanvas constructor
+caninit = r.TCanvas.__init__
+
+# define a new TCanvas class (inheriting from the original one),
+# setting the memory ownership in the constructor
+class GarbageCollectionResistentCanvas(r.TCanvas):
+  def __init__(self, *args):
+    caninit(self,*args)
+    r.SetOwnership(self,False)
+
+# replace the old TCanvas class by the new one
+r.TCanvas = GarbageCollectionResistentCanvas
+
+# keep a pointer to the original TPad constructor
+padinit = r.TPad.__init__
+
+# define a new TPad class (inheriting from the original one),
+# setting the memory ownership in the constructor
+class GarbageCollectionResistentCanvas(r.TPad):
+  def __init__(self, *args):
+    padinit(self,*args)
+    r.SetOwnership(self,False)
+
+# replace the old TPad class by the new one
+r.TPad = GarbageCollectionResistentCanvas
+
+# keep a pointer to the original TH1D constructor
+th1dinit = r.TH1D.__init__
+
+# define a new TH1D class (inheriting from the original one),
+# setting the memory ownership in the constructor
+class GarbageCollectionResistentH1D(r.TH1D):
+  def __init__(self, *args):
+    th1dinit(self,*args)
+    r.SetOwnership(self,False)
+
+# replace the old TH1D class by the new one
+r.TH1D = GarbageCollectionResistentH1D
+
+# keep a pointer to the original TLegend constructor
+leginit = r.TLegend.__init__
+
+# define a new TLegend class (inheriting from the original one),
+# setting the memory ownership in the constructor
+class GarbageCollectionResistentLegend(r.TLegend):
+  def __init__(self, *args):
+    leginit(self,*args)
+    r.SetOwnership(self,False)
+
+# replace the old TLegend class by the new one
+r.TLegend = GarbageCollectionResistentLegend
+
+# keep a pointer to the original TFile constructor
+fileinit = r.TFile.__init__
+
+# define a new TFile class (inheriting from the original one),
+# setting the memory ownership in the constructor
+class GarbageCollectionResistentFile(r.TFile):
+  def __init__(self, *args):
+    fileinit(self,*args)
+    r.SetOwnership(self,False)
+
+# replace the old TFile class by the new one
+r.TFile = GarbageCollectionResistentFile
+
 def makeNicePlot(hist,params,debug=False):
     """makeNicePlot: takes a histogram object and a set of parameters and applies the settings
     params: a map of parameters
