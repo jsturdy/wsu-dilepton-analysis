f4d6d1cf9c0f618c21ed0e44033bb9a5da5992a5
Jared Sturdy <sturdy@cern.ch>
2017-09-21 18:19:40 +0200

    Fixed some compile time errors, by slight redesign

--- GenLevelFilter/python/genLevelFilter_cfi.py
+++ GenLevelFilter/python/genLevelFilter_cfi.py
@@ -4,18 +4,10 @@ import FWCore.ParameterSet.Config as cms
 genLevelFilter = cms.EDFilter('GenLevelFilter',
     genParticleSource = cms.InputTag('prunedGenParticles'),
     filterevent       = cms.bool(False),
-    debug             = cms.bool(False),
-    minCut            = cms.double(-1.),
-    maxCut            = cms.double(1e8),
-    sampleType        = cms.string(""),
-)
-
-genLevelFilterP = cms.EDProducer('GenLevelFilter',
-    genParticleSource = cms.InputTag('prunedGenParticles'),
-    filterevent       = cms.bool(False),
     filterPreFSR      = cms.bool(False),
     debug             = cms.bool(False),
     minCut            = cms.double(-1.),
     maxCut            = cms.double(1e8),
+    xsWeight          = cms.double(-1),
     sampleType        = cms.string(""),
 )
